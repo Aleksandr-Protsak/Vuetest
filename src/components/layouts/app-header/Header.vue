a65227829ec55da62d8641ca45e80cca47ea42ef
Aleksandr-Protsak <45597787+Aleksandr-Protsak@users.noreply.github.com>
2019-02-21 20:41:44 +0200

    Revert "sssss"
    This reverts commit 1d666cad44522a88d599de07fed75216968a24e1.

--- src/components/layouts/app-header/Header.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <header class="appheader">
-        <div class="Logo">
-            <img class="imglogo" src="@/assets/logo.png"/>
-            <p class="logo_text">CoinMarketCap</p>
-        </div>
-    </header>
-</template>
